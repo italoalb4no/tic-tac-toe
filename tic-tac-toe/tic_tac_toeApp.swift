e1fd903ba43a379aa5f073c78146cf47e2b3b545
italoalb4no <italoclashalbano@gmail.com>
2023-06-08 22:54:16 +0200

    Initial Commit

---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Italo Albano on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
